1fbabe3471865dd269ded2a54c73ff0c827db790
GlorifiedPig <killingpigs123@gmail.com>
2020-04-04 15:13:38 +0200

    reformat headbobbing

--- Assets/Scripts/CharacterEffects/HeadBob.cs
+++ Assets/Scripts/CharacterEffects/HeadBob.cs
@@ -22,29 +22,24 @@ public class HeadBob : MonoBehaviour
 
         Vector3 cSharpConversion = transform.localPosition;
 
-        if (Mathf.Abs(horizontal) == 0 && Mathf.Abs(vertical) == 0)
-        {
+        if ( Mathf.Abs( horizontal ) == 0 && Mathf.Abs( vertical ) == 0) {
             timer = 0.0f;
-        }
-        else
-        {
-            waveslice = Mathf.Sin(timer);
+        } else {
+            waveslice = Mathf.Sin( timer );
             timer = timer + bobbingSpeed;
-            if (timer > Mathf.PI * 2)
+            if ( timer > Mathf.PI * 2 )
             {
-                timer = timer - (Mathf.PI * 2);
+                timer = timer - ( Mathf.PI * 2 );
             }
         }
-        if (waveslice != 0)
-        {
+
+        if ( waveslice != 0 ) {
             float translateChange = waveslice * bobbingAmount;
-            float totalAxes = Mathf.Abs(horizontal) + Mathf.Abs(vertical);
-            totalAxes = Mathf.Clamp(totalAxes, 0.0f, 1.0f);
+            float totalAxes = Mathf.Abs( horizontal ) + Mathf.Abs( vertical );
+            totalAxes = Mathf.Clamp( totalAxes, 0.0f, 1.0f );
             translateChange = totalAxes * translateChange;
             cSharpConversion.y = midpoint + translateChange;
-        }
-        else
-        {
+        } else {
             cSharpConversion.y = midpoint;
         }
 
