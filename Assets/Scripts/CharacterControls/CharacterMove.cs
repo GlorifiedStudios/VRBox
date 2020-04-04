098321c2e842acda7eec7e888483094d8f58ea6c
GlorifiedPig <killingpigs123@gmail.com>
2020-04-04 15:08:28 +0200

    initial commit

---
+++ Assets/Scripts/CharacterControls/CharacterMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMove : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed = 6f;
+
+    private CharacterController charController;
+
+    private void Awake()
+    {
+        charController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        PlayerMovement();
+    }
+
+    private void PlayerMovement()
+    {
+        float horizInput = Input.GetAxis( "Horizontal" ) * movementSpeed;
+        float vertInput = Input.GetAxis( "Vertical" ) * movementSpeed;
+
+        Vector3 forwardMovement = transform.forward * vertInput;
+        Vector3 rightMovement = transform.right * horizInput;
+
+        charController.SimpleMove(forwardMovement + rightMovement);
+    }
+}
---
+++ Assets/Scripts/GameManager/FrameLimiter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrameLimiter : MonoBehaviour
+{
+    private readonly int framerateLimit = 67;
+    
+    void Awake()
+    {
+        QualitySettings.vSyncCount = 0;  // VSync must be disabled
+        Application.targetFrameRate = framerateLimit;
+    }
+}
