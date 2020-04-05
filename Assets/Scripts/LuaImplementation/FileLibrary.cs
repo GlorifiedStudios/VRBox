2b85ef3104da038396e1f5ff08f1c5329efd6b16
GlorifiedPig <killingpigs123@gmail.com>
2020-04-05 17:24:56 +0200

    FileExists security measures (restricted to data folder)

--- Assets/Scripts/LuaImplementation/FileLibrary.cs
+++ Assets/Scripts/LuaImplementation/FileLibrary.cs
@@ -5,6 +5,15 @@ using MoonSharp.Interpreter;
 public class FileLibrary : MonoBehaviour
 {
     public static DynValue FileExists( string path ) {
+        string dataPath = Path.Combine( Application.streamingAssetsPath, "data" );
+        path = Path.Combine( dataPath, path );
+        Debug.Log( path );
+
+        if( path.Length < dataPath.Length || path.Substring( 0, dataPath.Length ) != dataPath ) {
+            Debug.Log( "Path out of data folder!" );
+            return DynValue.NewBoolean( false );
+        }
+
         bool fileExists = false;
         if( Directory.Exists( path ) ) { fileExists = true; }
         if( File.Exists( path ) ) { fileExists = true; }
