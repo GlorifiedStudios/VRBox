1ea62f5c73d98c61b30a0863df6455c637a8d581
GlorifiedPig <killingpigs123@gmail.com>
2020-04-05 15:07:23 +0200

    file structuring

---
+++ Assets/Scripts/LuaImplementation/FileLibrary.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using MoonSharp.Interpreter;
+using MoonSharp.Interpreter.Loaders;
+
+public class FileLibrary : MonoBehaviour
+{
+    public static DynValue FileExists( string path ) {
+        bool fileExists = false;
+        if( Directory.Exists( path ) ) { fileExists = true; }
+        if( File.Exists( path ) ) { fileExists = true; }
+        return DynValue.NewBoolean( fileExists );
+    }
+}
