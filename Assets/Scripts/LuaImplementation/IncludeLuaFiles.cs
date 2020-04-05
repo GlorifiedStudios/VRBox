5914a3eb6c8a82c3afae3f0ad575420fb9ff9ffe
GlorifiedPig <killingpigs123@gmail.com>
2020-04-05 12:11:12 +0200

    unwriteable globals

--- Assets/Scripts/LuaImplementation/IncludeLuaFiles.cs
+++ Assets/Scripts/LuaImplementation/IncludeLuaFiles.cs
@@ -7,15 +7,26 @@ using MoonSharp.Interpreter;
 
 public class IncludeLuaFiles : MonoBehaviour
 {
+    private static string[] unwriteableGlobals = new string[] {
+        "PushGlobal",
+        "GetGlobal"
+    };
+    
     private static Dictionary<string, DynValue> luaGlobals = new Dictionary<string, DynValue>();
 
     private static DynValue PushGlobal( string identifier, DynValue value ) {
+        if( unwriteableGlobals.Contains( identifier ) ) { return DynValue.NewNil(); }
         luaGlobals[identifier] = value;
         return value;
     }
 
+    private static DynValue GetGlobal( string identifier ) {
+        return luaGlobals[identifier];
+    }
+
     private void AssignLuaGlobals( Script luaScript ) {
         luaScript.Globals["PushGlobal"] = (Func<string, DynValue, DynValue>)PushGlobal;
+        luaScript.Globals["GetGlobal"] = (Func<string, DynValue>)GetGlobal;
         foreach( KeyValuePair<string, DynValue> globalString in luaGlobals ) {
             luaScript.Globals[globalString.Key] = globalString.Value;
         }
