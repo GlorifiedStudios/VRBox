f738c3048f34bbff22aa63ea659b8295c42fcd12
GlorifiedPig <killingpigs123@gmail.com>
2020-04-05 17:40:15 +0200

    print compatibility + setup load order

---
+++ Assets/Scripts/LuaImplementation/LuaScriptSetup.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using MoonSharp.Interpreter;
+using MoonSharp.Interpreter.Loaders;
+
+public class LuaScriptSetup : MonoBehaviour
+{
+    void Start()
+    {
+        Script.DefaultOptions.ScriptLoader = new FileSystemScriptLoader();
+        Script.DefaultOptions.DebugPrint = s => Debug.Log( s );
+    }
+}
