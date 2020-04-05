ca270be7b316aaaed1271a36333b12b9823d0df4
GlorifiedPig <killingpigs123@gmail.com>
2020-04-05 11:13:26 +0200

    basic Lua functionality with MoonSharp

---
+++ Assets/Scripts/LuaImplementation/NativeFiles.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MoonSharp.Interpreter;
+
+public class NativeFiles : MonoBehaviour
+{
+    void Start()
+    {
+        string filePath = System.IO.Path.Combine( Application.streamingAssetsPath, "Modules" );
+        filePath = System.IO.Path.Combine( filePath, "Native" );
+        filePath = System.IO.Path.Combine( filePath, "Native.lua" );
+
+        DynValue luaOutput = Script.RunString( System.IO.File.ReadAllText( filePath ) );
+        Debug.Log( luaOutput );
+    }
+}
