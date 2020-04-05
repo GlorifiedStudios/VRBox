51b3af18cf4aec39f4258bec7b681ec297ed4cf7
GlorifiedPig <killingpigs123@gmail.com>
2020-04-05 11:24:48 +0200

    auto include all lua modules

---
+++ Assets/Scripts/LuaImplementation/IncludeLuaFiles.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using MoonSharp.Interpreter;
+
+public class IncludeLuaFiles : MonoBehaviour
+{
+    void Start()
+    {
+        string modulePath = Path.Combine( Application.streamingAssetsPath, "Modules" );
+        string[] allModules = Directory.GetFiles( modulePath, "*.*", SearchOption.AllDirectories );
+        foreach ( var file in allModules ){
+            if( file.Substring( Mathf.Max( 0, file.Length - 4 ) ) == ".lua" ) {
+                DynValue luaOutput = Script.RunString( File.ReadAllText( file ) );
+                Debug.Log( luaOutput );
+            }
+        }
+    }
+}
