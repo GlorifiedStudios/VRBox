f3d3ab70eba12d35757d68e430afb8101e302f82
GlorifiedPig <killingpigs123@gmail.com>
2020-04-05 17:28:49 +0200

    less variable usage

--- Assets/Scripts/LuaImplementation/FileLibrary.cs
+++ Assets/Scripts/LuaImplementation/FileLibrary.cs
@@ -7,7 +7,6 @@ public class FileLibrary : MonoBehaviour
     public static DynValue FileExists( string path ) {
         string dataPath = Path.Combine( Application.streamingAssetsPath, "data" );
         path = Path.Combine( dataPath, path );
-        Debug.Log( path );
 
         if( path.Length < dataPath.Length || path.Substring( 0, dataPath.Length ) != dataPath ) {
             Debug.Log( "Path out of data folder!" );
--- Assets/Scripts/LuaImplementation/LuaScriptLoader.cs
+++ Assets/Scripts/LuaImplementation/LuaScriptLoader.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 using MoonSharp.Interpreter;
@@ -11,23 +10,16 @@ public class LuaScriptLoader : MonoBehaviour
         luaScript.Globals["FileExists"] = (Func<string, DynValue>)FileLibrary.FileExists;
     }
 
-    void Start()
-    {
-        Script.DefaultOptions.ScriptLoader = new FileSystemScriptLoader();
-
+    private void LoadAutorunFiles() {
         string modulesPath = Path.Combine( Application.streamingAssetsPath, "modules" );
-        string[] moduleFolders = Directory.GetDirectories( modulesPath, "*.*", SearchOption.AllDirectories );
-        foreach( var folder in moduleFolders ) {
+        foreach( var folder in Directory.GetDirectories( modulesPath, "*.*", SearchOption.AllDirectories ) ) {
             string moduleFolder = Path.Combine( modulesPath, folder + "/lua" );
             string autorunFolder = Path.Combine( moduleFolder, "autorun" );
             if( Directory.Exists( autorunFolder ) ) {
-                string[] autorunFiles = Directory.GetFiles( autorunFolder, "*.*", SearchOption.AllDirectories );
-                foreach ( var file in autorunFiles ) {
+                foreach ( var file in Directory.GetFiles( autorunFolder, "*.*", SearchOption.AllDirectories ) ) {
                     if( file.Substring( Mathf.Max( 0, file.Length - 4 ) ) == ".lua" ) {
                         Script luaScript = new Script();
-                        
                         ((ScriptLoaderBase)luaScript.Options.ScriptLoader).ModulePaths = ScriptLoaderBase.UnpackStringPaths( moduleFolder + "/?;" + moduleFolder + "/?.lua" );
-
                         AssignLuaGlobals( luaScript );
                         DynValue luaOutput = luaScript.DoFile( file );
                         Debug.Log( luaOutput );
@@ -35,5 +27,10 @@ public class LuaScriptLoader : MonoBehaviour
                 }
             }
         }
+    }
+
+    void Start() {
+        Script.DefaultOptions.ScriptLoader = new FileSystemScriptLoader();
+        LoadAutorunFiles();
     }
 }
