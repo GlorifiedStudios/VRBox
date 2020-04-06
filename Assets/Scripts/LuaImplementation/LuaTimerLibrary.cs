5a735f2145ee37e09e61c4f90f1050b178a0a54d
GlorifiedPig <killingpigs123@gmail.com>
2020-04-06 02:12:01 +0200

    much better method of api communication

---
+++ Assets/Scripts/LuaImplementation/LuaTimerLibrary.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using UnityEngine;
+using MoonSharp.Interpreter;
+
+[MoonSharpUserData]
+class LuaTimerLibrary
+{
+    private IEnumerator Start( float seconds, DynValue function, Script luaScript ) {
+        yield return new WaitForSeconds( seconds );
+        luaScript.Call( function );
+    }
+}
