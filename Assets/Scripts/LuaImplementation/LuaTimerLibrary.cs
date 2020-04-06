56ed673decf13ec7b2106a345effb54611f249a3
GlorifiedPig <killingpigs123@gmail.com>
2020-04-06 03:02:09 +0200

    identifiers, repeat count and remove

--- Assets/Scripts/LuaImplementation/LuaTimerLibrary.cs
+++ Assets/Scripts/LuaImplementation/LuaTimerLibrary.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using MoonSharp.Interpreter;
 
@@ -9,8 +10,14 @@ class LuaTimerLibrary
         GameObject.FindGameObjectWithTag( "GameController" ).GetComponent<LuaScriptLoader>().StartCoroutine( StartEnumerator( seconds, function ) );
     }
 
-    public void Repeating( float seconds, Closure function ) {
-        GameObject.FindGameObjectWithTag( "GameController" ).GetComponent<LuaScriptLoader>().StartCoroutine( StartRepeatingEnumerator( seconds, function ) );
+    [MoonSharpHidden] Dictionary<string, int> repeatingTimers = new Dictionary<string, int>();
+    public void Repeating( string identifier, float seconds, int repetitions, Closure function ) {
+        repeatingTimers[identifier] = repetitions;
+        GameObject.FindGameObjectWithTag( "GameController" ).GetComponent<LuaScriptLoader>().StartCoroutine( StartRepeatingEnumerator( identifier, seconds, function ) );
+    }
+
+    public void Remove( string identifier ) {
+        repeatingTimers.Remove( identifier );
     }
 
     [MoonSharpHidden]
@@ -21,8 +28,10 @@ class LuaTimerLibrary
     }
 
     [MoonSharpHidden]
-    private IEnumerator StartRepeatingEnumerator( float seconds, Closure function ) {
-        while( true ) {
+    private IEnumerator StartRepeatingEnumerator( string identifier, float seconds, Closure function ) {
+        while( repeatingTimers.ContainsKey( identifier ) && repeatingTimers[identifier] > 0 ) {
+            repeatingTimers[identifier] = repeatingTimers[identifier] - 1;
+            if( repeatingTimers[identifier] <= 0 ) { repeatingTimers.Remove( identifier ); }
             yield return new WaitForSeconds( seconds );
             Script luaScript = function.OwnerScript;
             luaScript.Call( function );
