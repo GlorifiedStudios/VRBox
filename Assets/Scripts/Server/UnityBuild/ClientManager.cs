c0c31250d766806c4681b8f6ff47769f7d9616da
GlorifiedPig <killingpigs123@gmail.com>
2020-04-19 15:38:43 +0200

    progress on client-end

---
+++ Assets/Scripts/Server/UnityBuild/ClientManager.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+using System;
+using UnityEngine.UI;
+
+public class ClientManager : MonoBehaviour
+{
+    public static ClientManager instance;
+    public static int dataBufferSize = 4096;
+    public InputField ipInput = null;
+
+    public string ip = "127.0.0.1";
+    public int port = 34197;
+    public TCP tcp;
+
+    private void Awake()
+    {
+        if( ipInput.text != null ) { ip = ipInput.text; }
+
+        if( instance == null ) {
+            instance = this;
+        } else if (instance != this) {
+            Debug.Log( "Instance already exists, destroying object!" );
+            Destroy( this );
+        }
+    }
+
+    private void Start()
+    {
+        tcp = new TCP();
+    }
+
+    public void ConnectToServer()
+    {
+        tcp.Connect();
+    }
+
+    public class TCP
+    {
+        public TcpClient socket;
+
+        private NetworkStream stream;
+        private byte[] receiveBuffer;
+
+        public void Connect()
+        {
+            socket = new TcpClient
+            {
+                ReceiveBufferSize = dataBufferSize,
+                SendBufferSize = dataBufferSize
+            };
+
+            receiveBuffer = new byte[dataBufferSize];
+            socket.BeginConnect( instance.ip, instance.port, ConnectCallback, socket );
+        }
+
+        private void ConnectCallback(IAsyncResult _result)
+        {
+            socket.EndConnect(_result);
+
+            if (!socket.Connected)
+            {
+                return;
+            }
+
+            stream = socket.GetStream();
+
+            stream.BeginRead( receiveBuffer, 0, dataBufferSize, ReceiveCallback, null );
+        }
+
+        private void ReceiveCallback(IAsyncResult _result)
+        {
+            try
+            {
+                int _byteLength = stream.EndRead(_result);
+                if (_byteLength <= 0)
+                {
+                    return;
+                }
+
+                byte[] _data = new byte[_byteLength];
+                Array.Copy(receiveBuffer, _data, _byteLength);
+
+                stream.BeginRead(receiveBuffer, 0, dataBufferSize, ReceiveCallback, null);
+            }
+            catch
+            {
+                
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Server/UnityBuild/ServerManager.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class ServerManager : MonoBehaviour
+{
+    void Start()
+    {
+        Server.Start( 4, 34197 );
+    }
+}
