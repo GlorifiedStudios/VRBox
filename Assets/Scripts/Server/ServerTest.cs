808e1d72c0fc74bef2a579312fa3fbcdf47de9e4
GlorifiedPig <killingpigs123@gmail.com>
2020-04-19 15:18:13 +0200

    start on server build

---
+++ Assets/Scripts/Server/ServerTest.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ServerTest : MonoBehaviour
+{
+    void Start()
+    {
+        Server.Start( 4, 34197 );
+    }
+}
