d865353d2d1fe459c9fc65af3779cea27a639fde
GlorifiedPig <killingpigs123@gmail.com>
2020-04-23 15:35:23 +0200

    change capitalization

---
+++ Assets/StreamingAssets/modules/native/lua/autorun/includes.lua
@@ -0,0 +1,13 @@
+
+require( "types" )
+require( "tableutil" )
+table.Print({
+    ["Bruh"] = {
+        ["Boy"] = 5,
+        ["Boy2"] = 4,
+        ["Boy3"] = {
+            "Bruh", "Bruh2"
+        }
+    },
+    ["Bruv"] = 6
+})
