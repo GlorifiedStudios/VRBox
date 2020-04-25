a6189e985806efcec65c8671895fb38609ff7455
Jack <jpstevenson.avi@gmail.com>
2020-04-25 09:21:01 -0700

    Changed client dict to client array (Needs to be tested)

--- Assets/Scripts/Server/Server.cs
+++ Assets/Scripts/Server/Server.cs
@@ -9,13 +9,14 @@ public class Server
     public static int MaxPlayers { get; private set; }
     public static int Port { get; private set; }
 
-    public static Dictionary<int, Client> clients = new Dictionary<int, Client>();
+    public static Client[] clients;
 
     private static TcpListener tcpListener;
 
     public static void Start( int maxPlayers, int port ) {
         MaxPlayers = maxPlayers;
         Port = port;
+        clients = new Client[MaxPlayers];
 
         Debug.Log( "Starting VRBox server..." );
         InitializeServerData();
@@ -43,7 +44,7 @@ public class Server
 
     private static void InitializeServerData() {
         for( int i = 1; i <= MaxPlayers; i++ ) {
-            clients.Add( i, new Client( i ) );
+            clients[i] = new Client( i );
         }
     }
 }
